82d05c6052bd0bf98a63722104068f1d391656f0
rwmarmol <rwmarmol@gmail.com>
2019-09-04 23:27:26 -0300

    Pipes Setup

---
+++ src/app/pipes/filter/filter.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: any, arg: any): any {
+    const filterResult = [];
+    for(const character of value ){
+      if ( 
+          character.first_name.toLowerCase().indexOf(arg.toLowerCase()) != -1
+          ||
+          character.last_name.toLowerCase().indexOf(arg.toLowerCase()) != -1
+         ) {
+        filterResult.push(character);
+      }
+    }
+    return filterResult;
+  }
+
+}
